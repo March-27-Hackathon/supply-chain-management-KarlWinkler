fb630b9dfe481c271bc95aa693ec45fba55721d9
LAREINA <79848427+LAREINA-JO@users.noreply.github.com>
2021-03-30 15:59:39 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,12 +1,13 @@
-
+//This class is used for recording all the information of items in the inventory.
 public class Item {
-
+	//all the information of items in the database
 	private String id;
 	private String type;
 	private String[] typeVariables;
 	private String price;
 	private String manuID;
 	
+	//constructor without arguments, just create and empty object
 	public Item() {
 		this.id = "";
 		this.type = "";
@@ -14,7 +15,7 @@ public class Item {
 		this.price = "";
 		this.manuID = "";
 	}
-
+	//constructor with five specified arguments, create a specified item
 	public Item(String id, String type,String[] variables ,String price, String manuID) {
 		this.id = id;
 		this.type = type;
@@ -23,6 +24,7 @@ public class Item {
 		this.manuID = manuID;
 	}
 
+	//getter of all private variables
 	public String getId() {
 		return id;
 	}
