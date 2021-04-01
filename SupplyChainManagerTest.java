09c688f03ddc403dab7ef2b28e07829aee21e30f
Max Botham <maxbotham@h198-228.wlan.ucalgary.ca>
2021-04-01 02:00:57 -0600

    selectItem tests

--- SupplyChainManagerTest.java
+++ SupplyChainManagerTest.java
@@ -66,23 +66,58 @@ public class SupplyChainManagerTest {
     assertTrue(SELECTITEM_MESSAGE, compareArrayList(returned, shouldEqual));
   }
 
-  /*@Test
-  public void selectItemTest() {
+  @Test
+  public void selectItemChairTest() {
     manager = new SupplyChainManager(DBURL, USERNAME, PASSWORD);
     manager.initializeConnection();
-    ArrayList<Item> returned = manager.selectItem("Traditional", "desk");
+    ArrayList<Item> returned = manager.selectItem("Mesh", "chair");
     ArrayList<Item> shouldEqual = new ArrayList<Item>(4);
+    String [] firstItem = {"Y", "N", "Y", "Y"};
+    String [] secondItem = {"Y", "N", "N", "N"};
+    String [] thirdItem = {"N", "N", "Y", "N"};
+    String [] fourthItem = {"N", "Y", "N", "Y"};
+    shouldEqual.add(new Item("C0942", "Mesh", firstItem, "100", "005"));
+    shouldEqual.add(new Item("C6748", "Mesh", secondItem, "75", "003"));
+    shouldEqual.add(new Item("C8138", "Mesh", thirdItem, "75", "005"));
+    shouldEqual.add(new Item("C9890", "Mesh", fourthItem, "50", "003"));
+    assertTrue(SELECTITEM_MESSAGE, compareArrayList(returned, shouldEqual));
+  }
+
+  @Test
+  public void selectItemFilingTest() {
+    manager = new SupplyChainManager(DBURL, USERNAME, PASSWORD);
+    manager.initializeConnection();
+    ArrayList<Item> returned = manager.selectItem("Large", "filing");
+    ArrayList<Item> shouldEqual = new ArrayList<Item>(5);
     String [] firstItem = {"N", "N", "Y"};
-    String [] secondItem = {"N", "Y", "Y"};
-    String [] thirdItem = {"Y", "Y", "N"};
-    String [] fourthItem = {"Y", "N", "Y"};
-    shouldEqual.add(new Item("D0890", "Traditional", firstItem, "25", "002"));
-    shouldEqual.add(new Item("D4231", "Traditional", secondItem, "50", "005"));
-    shouldEqual.add(new Item("D8675", "Traditional", thirdItem, "75", "001"));
-    shouldEqual.add(new Item("D9352", "Traditional", fourthItem, "75", "002"));
-    boolean doesMatch = compareArrayList(returned, shouldEqual);
-    assertTrue(SELECTITEM_MESSAGE, doesMatch);
-  }*/
+    String [] secondItem = {"Y", "N", "Y"};
+    String [] thirdItem = {"N", "Y", "Y"};
+    String [] fourthItem = {"N", "Y", "N"};
+    String [] fifthItem = {"Y", "N", "N"};
+    shouldEqual.add(new Item("F003", "Large", firstItem, "150", "002"));
+    shouldEqual.add(new Item("F010", "Large", secondItem, "225", "002"));
+    shouldEqual.add(new Item("F011", "Large", thirdItem, "225", "005"));
+    shouldEqual.add(new Item("F012", "Large", fourthItem, "75", "005"));
+    shouldEqual.add(new Item("F015", "Large", fifthItem, "75", "004"));
+    assertTrue(SELECTITEM_MESSAGE, compareArrayList(returned, shouldEqual));
+  }
+
+  @Test
+  public void selectItemLampTest() {
+    manager = new SupplyChainManager(DBURL, USERNAME, PASSWORD);
+    manager.initializeConnection();
+    ArrayList<Item> returned = manager.selectItem("Swing Arm", "lamp");
+    ArrayList<Item> shouldEqual = new ArrayList<Item>(4);
+    String [] firstItem = {"Y", "N"};
+    String [] secondItem = {"N", "Y"};
+    String [] thirdItem = {"Y", "N"};
+    String [] fourthItem = {"N", "Y"};
+    shouldEqual.add(new Item("L053", "Swing Arm", firstItem, "27", "002"));
+    shouldEqual.add(new Item("L096", "Swing Arm", secondItem, "3", "002"));
+    shouldEqual.add(new Item("L487", "Swing Arm", thirdItem, "27", "002"));
+    shouldEqual.add(new Item("L879", "Swing Arm", fourthItem, "3", "005"));
+    assertTrue(SELECTITEM_MESSAGE, compareArrayList(returned, shouldEqual));
+  }
 
   private boolean compareArrayList(ArrayList<Item> one, ArrayList<Item> two){
     if(one.size()!=two.size()){
