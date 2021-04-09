bdc21c281cc25476d4a2caba1c8d7f61dae2f4c8
zheng-628 <80284414+zheng-628@users.noreply.github.com>
2021-04-08 20:13:38 -0600

    Change a small error and limit 80 per line

--- OutFile.java
+++ OutFile.java
@@ -11,7 +11,8 @@ public class OutFile {
 	private String TotalPrice;
 	private static String filename ="OutputFile.txt";
 	
-	//constructor with four parameters, creates a new object with the specified request, quantity,
+	//constructor with four parameters, 
+	//creates a new object with the specified request, quantity,
 	//ID, and price
 	/**
 	 * Constructor for the OutFile object with four parameters
@@ -27,7 +28,8 @@ public class OutFile {
 		this.TotalPrice=Integer.toString(prc);
 	}
 	
-	//constructor with three parameters, creates a new object with the specified request, quantity, and companies
+	//constructor with three parameters, 
+	//creates a new object with the specified request, quantity, and companies
 	/**
 	 * Constructor for the OutFile object with three parameters
 	 * @param requested furniture
@@ -53,7 +55,8 @@ public class OutFile {
 		filewrite.write("\n"+"Faculty Name:"+"\n");
 		filewrite.write("Contact:"+"\n");
 		filewrite.write("Date:"+"\n");
-		filewrite.write("\n"+"Original Request: "+ Request +", " +Quantity +"\n");
+		filewrite.write("\n"+"Original Request: "
+						+ Request +", " +Quantity +"\n");
 		filewrite.write("\n"+"Items Ordered"+"\n");
 		for(String id : IDs){
 			filewrite.write("ID: "+ id + "\n");
@@ -62,8 +65,10 @@ public class OutFile {
 		filewrite.close();
 	}
 	
-	//method writeNoneAvailable with one argument (requried furniture's name), which 
-	//is used for writing in the file if there is no fulfilled result in the database
+	//method writeNoneAvailable with one argument 
+	//(requried furniture's name), 
+	//which is used for writing in the file 
+	//if there is no fulfilled result in the database
 	/**
 	 * Method to write the file when requested furnitures are not
 	 * available. The file generated has the companies' name which produce
@@ -76,29 +81,34 @@ public class OutFile {
 		filewrite.write("\n"+"Faculty Name:"+"\n");
 		filewrite.write("Contact:"+"\n");
 		filewrite.write("Date:"+"\n");
-		filewrite.write("\n"+"Original Request: "+ name + " " + Request +", " + Quantity +"\n");
-		filewrite.write("\n"+"Order cannot be fulfilled based on current inventory. Suggested manufacturers are ");
+		filewrite.write("\n"+"Original Request: "+ Request 
+						+ " " +", " + Quantity +"\n");
+		filewrite.write("\n"+"Order cannot be fulfilled "+
+							"based on current inventory."+
+						" Suggested manufacturers are ");
 		
-		//If there is no fulfilled result, the system will suggest some other available manufacturers
+		//If there is no fulfilled result, 
+		//the system will suggest some other available manufacturers
 		String ManNotIncluded1="";
 		String ManNotIncluded2="";
-		if(Request.equals("chair")||Request.equals("Chair")){
+		if(name.equals("chair")||name.equals("Chair")){
 			ManNotIncluded1="Academic Desks";
 		}
-		else if(Request.equals("desk")||Request.equals("Desk")){
+		else if(name.equals("desk")||name.equals("Desk")){
 			ManNotIncluded1="Chairs R Us";
 		}
-		else if(Request.equals("lamp")||Request.equals("Lamp")){
+		else if(name.equals("lamp")||name.equals("Lamp")){
 			ManNotIncluded1="Academic Desks";
 			ManNotIncluded2="Chairs R Us";
 		}
-		else if(Request.equals("filing")||Request.equals("Filing")){
+		else if(name.equals("filing")||name.equals("Filing")){
 			ManNotIncluded1="Academic Desks";
 			ManNotIncluded2="Chairs R Us";
 		}
 		ArrayList <String> contactMan = new ArrayList <String> ();
 		for(int i = 0; i < companies.size(); i++){
-			if(companies.size() > 0 && !(companies.get(i).equals(ManNotIncluded1)) && !(companies.get(i).equals(ManNotIncluded2) )){
+			if(companies.size() > 0 && !(companies.get(i).equals(ManNotIncluded1)) 
+				&& !(companies.get(i).equals(ManNotIncluded2) )){
 				contactMan.add(companies.get(i));
 				contactMan.add(", ");
 			}
@@ -110,7 +120,7 @@ public class OutFile {
 		filewrite.close();
 	}
 	
-	/* test
+	/*
 	public static void main (String [] args) throws IOException{
 		ArrayList <String> ts=new ArrayList <String> ();
 		ts.add("Academic Desks");
@@ -118,8 +128,10 @@ public class OutFile {
 		ts.add("Chairs R Us");
 		ts.add("Furniture Goods");
 		ts.add("Fine Office Supplies");
-		OutFile test =new OutFile ("desk","2",ts);
-		test.writeNoneAvailable();
-	}*/
+		OutFile test =new OutFile ("mesh chair","2",ts);
+		test.writeNoneAvailable("chair");
+	}
+	*/
+	
 	
 }
